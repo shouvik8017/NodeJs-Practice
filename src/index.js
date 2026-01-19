1f33e8914ec1cf94f90d911187426dbd53c3ee4b
shouvik8017 <shouvik.webgen@gmail.com>
2026-01-19 13:20:49 +0530

    utils code setup done

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import connectDB from "./db/index.js";
 
 config();
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err) => {
+    console.log(`Mongo DB connection failed - ${err}`)
+});
